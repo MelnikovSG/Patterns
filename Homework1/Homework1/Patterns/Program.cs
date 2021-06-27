8dd6d7a50498f8bfea72d9ffbbce4b0980b0eb0a
MelnikovSG <57829903+MelnikovSG@users.noreply.github.com>
2021-06-27 17:28:10 +0700

    Homework1

---
+++ Homework1/Homework1/Patterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте вас приветствует математическая программа");
+            InputSolution.EnterNumber();
+            Console.ReadLine();
+        }
+    }
+}
